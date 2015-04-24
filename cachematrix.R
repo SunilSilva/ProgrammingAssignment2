be2e2e39034ea109a8c4753cad712b954e80e22a
SunilSilva <nadeep111@hotmail.com>
2015-04-25 07:52:16 +1000

    Added the first part of the Asgn2

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,30 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do:
 
-## Write a short comment describing this function
+## the function is supposed to calculate the inverse of a given matrix
+## the first function returns a list with four functions being elements
+## the second function check any previous calculation and the calculates the 
+## inverse if it is null
+
+## Write a short comment describing this function:
+## this function returns a list with four elements
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, 
+             get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
+
+
 
 
 ## Write a short comment describing this function
