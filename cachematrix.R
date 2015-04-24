2c97c1616e804a0dfd60fc590dc883ca8f91c679
SunilSilva <nadeep111@hotmail.com>
2015-04-25 08:41:02 +1000

    Completed

--- cachematrix.R
+++ cachematrix.R
@@ -10,14 +10,21 @@
 ## this function returns a list with four elements
 
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
+        m<-NULL   # inversion result is stored
+        
+        # A setter function holds the objects from makeCacheMatrix
         set<-function(y){
                 x<<-y
+                ##initialises xinv to null
                 m<<-NULL
         }
-        get<-function() x
+        # return the input matrix
+        get<-function() x 
+        # set the inversed matrix
         setmatrix<-function(solve) m<<- solve
+        # return the inversed matrix
         getmatrix<-function() m
+        # return a list with elements being four functions
         list(set=set, 
              get=get,
              setmatrix=setmatrix,
@@ -35,16 +42,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ## returns the inverse of a given matrix
 
 cacheSolve <- function(x=matrix(), ...) {
+        # get the inversed matrix
         m<-x$getmatrix()
+        # check inverse is null
         if(!is.null(m)){
                 message("getting cached data")
                 return(m)
         }
+        # get matrix  
         matrix<-x$get()
+        # use R function solve to get the inverse
         m<-solve(matrix, ...)
+        # set the result
         x$setmatrix(m)
+        #return the results
         m
 }
 
+## example
 ##a<-makeCacheMatrix(matrix(2:5,2,2))
 ##cacheSolve(a) 
